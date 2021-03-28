ee2fb0e9b151da26ec1d87f3514007cb55510249
albert-santos01 <albert.santos01@estudiant.upf.edu>
2021-03-28 17:07:58 -0400

    prepostputa2

--- src/sources/state.c
+++ src/sources/state.c
@@ -3,13 +3,13 @@
 #include "../headers/state.h"
 
 /**
- * TODO: Initializes the state, setting the playing board, the current position to 0 and finished flag to FALSE.
+ *  Initializes the state, setting the playing board, the current position to 0 and finished flag to FALSE.
  *
  * @param state The state to be initialized.
  * @param board The playing board.
  *
- * Pre:
- * Post:
+ * Pre: El board deberia estar inicializado
+ * Post:Dado el state copia el board dado, pone la position como 0 y el finishe como FALSE
  */
 void init_state(State* state, Board* board) {
     state->board=board;
@@ -19,58 +19,58 @@ void init_state(State* state, Board* board) {
 }
 
 /**
- * TODO: Sets the current position.
+ * Sets the current position.
  *
  * @param state The state to be updated.
  * @param position The current position.
  *
- * Pre:
- * Post:
+ * Pre: La position deberia ser un número entre las medidas del tablero
+ * Post: Se actualiza el state con la posicion dada
  */
 void set_current_position(State* state, int position) {
     state->position = position;
 }
 
 /**
- * TODO: Retrieves the current position.
+ *  Retrieves the current position.
  *
  * @param state The state that stores the current position.
  * @return The current position.
  *
- * Pre:
- * Post:
+ * Pre: el state debe estar al menos inicializado
+ * Post:Se devuelve la position que contiene el state
  */
 int get_current_position(State* state) {
     return state->position;
 }
 
 /**
- * TODO: Sets the finished flag to finished value (TRUE or FALSE)
+ * Sets the finished flag to finished value (TRUE or FALSE)
  *
  * @param state The state to be updated.
  * @param is_finished The value marking the game as finished or not.
  *
- * Pre:
- * Post:
+ * Pre: el state debe estar al menos inicializado y finished deberia ser o TRUE o FALSE
+ * Post: el state  es actualizado con el finished dado
  */
 void set_finished(State* state, int finished) {
     state->finished = finished;
 }
 
 /**
- * TODO: Returns the value of is_finished flag (TRUE or FALSE), marking a game as finished.
+ *  Returns the value of is_finished flag (TRUE or FALSE), marking a game as finished.
  *
  * @param state The state to be updated.
  *
- * Pre:
- * Post:
+ * Pre: el state debe estar al menos inicializado
+ * Post:Devuelve el valor del finished del state dado
  */
 int is_finished(State* state) {
     return state->finished;
 }
 
 /**
- * TODO: Moves the player a number of steps in the board, updating the state's current position. If the player ends up in a
+ *  Moves the player a number of steps in the board, updating the state's current position. If the player ends up in a
  * square containing a ladder or a snake updates the state's current position accordingly. Also, sets the state as
  * finished if it moves to or past the last square. Returns SNAKE_FOUND or LADDER_FOUND if found, SUCCESS otherwise.
  *
@@ -78,11 +78,13 @@ int is_finished(State* state) {
  * @param state The current state.
  * @param steps The number of steps to move.
  *
- * Pre:
- * Post:
+ * Pre: el state debe estar almenos inicializado y el dice_value se recomienda que este entre 1 y 6
+ * Post: La función devuelve  LADDER_FOUND si se ha encontrado una escalera, SNAKE_FOUND si se ha encontrado una serpiente o un 20
+ *      si ninguno de los dos casos. El state será actualizado con la nueva posición y si la nueva posición es más grande que el tamaño
+ *      del tablero el finished pasa a ser  TRUE.
  */
 int move(State* state, int dice_value) {
-    int status;
+    int status=20;
     Board* board=state->board; // this is the address
     int board_size= get_size(board);
     int current_position= get_current_position(state);
@@ -96,8 +98,8 @@ int move(State* state, int dice_value) {
             status = SNAKE_FOUND;
             current_position= get_target_position(current_square);
         }
-    } else if(current_position>board_size-2) {//ESTO ES QUE HA GANADO SIN CONTAR EL REBOTE
-        current_position = board_size - 2;
+    } else if(current_position>board_size-1) {//ESTO ES QUE HA GANADO SIN CONTAR EL REBOTE
+        current_position = board_size - 1;
         set_finished(state, TRUE);
     }
     set_current_position(state,current_position);
@@ -105,10 +107,12 @@ int move(State* state, int dice_value) {
 }
 
 /**
- * TODO: Adds a step to the state's sequence, as the last step of it.
+ * Adds a step to the state's sequence, as the last step of it.
  *
- * Pre:
- * Post:
+ * Pre:  el state debe estar almenos inicializado y el dice_value se recomienda que este entre 1 y 6 y la sequencia debe ser una lista
+ *      bien enlazada
+ * Post: Crea el step dinámicamente con los datos dados y lo añade alfinal de la sequence. Si es el primer step de la sequence será el
+ *       primero y el último.
  */
 void add_step(State* state, int dice_value) {
     add_step_as_last(&state->sequence,state->position,dice_value); //CADA VEZ QUE AGREGAMOS UN PASO LO PONE COMO LAST
@@ -119,9 +123,10 @@ void add_step(State* state, int dice_value) {
  * Prints the sequence of the state.
  * @param sequence The sequence to be shown.
  *
- * Pre:
- * Post:
- */
+ * Pre: el state debe estar almenos inicializado y la sequencia debe ser una lista
+ *      bien enlazada
+ * Post: imprime los movimientos  en orden que guarda la sequence
+ * */
 void print_state_sequence(State* state) {
    print_sequence(&state->sequence);
 }
